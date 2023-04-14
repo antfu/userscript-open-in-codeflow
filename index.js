d3ae6699515cd3f4ef16ad5e2e8fb93e36684e6f
patak <matias.capeletto@gmail.com>
2023-04-14 16:24:21 +0200

    fix: also match url with anchors

--- index.js
+++ index.js
@@ -8,6 +8,7 @@
 // @author       Anthony Fu
 // @license      MIT
 // @match        https://github.com/**
+// @match        https://github.com/**#**
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=pr.new
 // @grant        none
 // ==/UserScript==
