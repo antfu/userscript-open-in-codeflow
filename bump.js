35344376fcd403624857d7790a29cb7be17f4d13
Chris <hizyyv@gmail.com>
2026-09-04 16:07:10 +0800

    fix: update dom query logic & udpate deps (#3)

--- bump.js
+++ bump.js
@@ -1,6 +1,10 @@
 import fs from 'node:fs/promises'
 
-const { version } = JSON.parse(await fs.readFile('package.json', 'utf8'))
-const content = await fs.readFile('index.js', 'utf8')
-const newContent = content.replace(/\/\/ @version\s+.*$/m, `// @version      ${version}`)
-await fs.writeFile('index.js', newContent, 'utf8')
+async function main() {
+  const { version } = JSON.parse(await fs.readFile('package.json', 'utf8'))
+  const content = await fs.readFile('index.js', 'utf8')
+  const newContent = content.replace(/\/\/ @version\s+(?:\S.*)?$/m, `// @version      ${version}`)
+  await fs.writeFile('index.js', newContent, 'utf8')
+}
+
+main()
